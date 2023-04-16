6f51e11a6675b85b9ca20e494b1b1be5994421cb
miya <sirotumesirokuma@gmail.com>
2023-04-16 09:01:37 +0900

    added RLS

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,10 @@
 import '@/styles/globals.css'
 import type { AppProps, NextWebVitalsMetric } from 'next/app'
+import {ReactQueryDevtools} from "react-query/devtools"
+import {QueryClient, QueryClientProvider} from "react-query"
+import { useRouter } from 'next/router'
+import { supabase } from '@/utils/supabase'
+import { useEffect } from 'react'
 
 export function reportWebVitals(metric: NextWebVitalsMetric) {
   switch (metric.name) {
@@ -23,7 +28,39 @@ export function reportWebVitals(metric: NextWebVitalsMetric) {
       break
   }
 }
+const queryClient = new QueryClient({
+  defaultOptions: {queries : {
+    retry: false,
+    refetchOnWindowFocus: false
+  }
+}
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const {push , pathname} = useRouter()
+  const validateSession = async () => {
+    const user = supabase.auth.user()
+    if(user && pathname === "/") {
+      push("/dashboard")
+    } else if (!user && pathname !== "/") {
+      await push("/")
+    }
+    supabase.auth.onAuthStateChange((event, _) => {
+      if (event === 'SIGNED_IN' && pathname === '/') {
+        push('/dashboard')
+      }
+      if (event === 'SIGNED_OUT') {
+        push('/')
+      }
+    })
+  }
+  useEffect(() => {
+    validateSession()
+  }, [])
+  return (
+  <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  )
 }
--- types/types.ts
+++ types/types.ts
@@ -1,13 +1,15 @@
 export type Task = {
   id: string;
-  create_at: string;
+  created_at: string;
   title: string;
   user_id: string | undefined;
 }
 
 export type Notice = {
   id: string;
-  create_at: string;
+  created_at: string;
   content: string;
   user_id: string | undefined;
 }
+export type EditedTask = Omit<Task, 'created_at' | 'user_id'>
+export type EditedNotice = Omit<Notice, 'created_at' | 'user_id'>
